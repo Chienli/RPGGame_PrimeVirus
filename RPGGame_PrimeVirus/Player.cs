a01dffbf3301000c0642cdcdba149944a6576a70
Guy Lin Chien Li <guy.lin@titansoft.com.sg>
2019-04-17 17:53:24 +0800

    new a class Player.cs

---
+++ RPGGame_PrimeVirus/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGGame_PrimeVirus
+{
+	internal class Player
+	{
+		public Player()
+		{
+			Console.WriteLine("Please enter your Name:");
+		}
+
+		public int Hp
+		{
+			get { return 100; }
+			set { Hp = value; }
+		}
+
+		public string Name { get; set; }
+	}
+}

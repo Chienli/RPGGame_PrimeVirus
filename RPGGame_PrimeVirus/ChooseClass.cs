26c20e62a527b316b1961c0ba731b5c17581ed0e
vincent.chen99 <vincent.chen99@TI03NB68>
2019-04-17 17:54:05 +0800

    新增選職業的Class

---
+++ RPGGame_PrimeVirus/ChooseClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGGame_PrimeVirus
+{
+    internal class ChooseClass
+    {
+        public void chooseClass(int x)
+        {
+            Console.WriteLine("type[Novice],[Warrior] or [Priest] to create your character");
+            var input = Console.ReadLine();
+            string[] job = { "Novice", "Warrior", "Priest" };
+            switch (input)
+            {
+                case "Novice": break;
+                case "Warrior": break;
+                case "Priest": break;
+            }
+        }
+    }
+}
